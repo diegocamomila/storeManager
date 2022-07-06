ee5c50ebba8ed4132616271f85b00eca744feb9a
Diego Siqueira <diego.camomila@gmail.com>
2022-07-06 20:51:27 -0300

    refactor: implementado funçao para lidar com erro

--- services/productsServices.js
+++ services/productsServices.js
@@ -1,8 +1,12 @@
 const productsModels = require('../models/productsModels');
 
+const treatsError = (status, message) => ({ status, message });
+
 const getAll = async () => {
   const rows = await productsModels.getAll();
-  if (!rows) return [];
+  if (rows.length < 1) {
+    throw treatsError(404, 'Products not found');
+  }
   return rows;
 };
 
