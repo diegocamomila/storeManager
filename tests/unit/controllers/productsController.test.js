36691903885d8c4bf49acf13b80e682baf244a2d
Diego Siqueira <diego.camomila@gmail.com>
2022-07-06 20:57:32 -0300

    feat: inpantaçao de productsControllers.test

---
+++ tests/unit/controllers/productsController.test.js
@@ -0,0 +1,64 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const productsControllers = require('../../../controllers/productsControllers');
+const  productsServices  = require('../../../services/productsServices');
+
+// Testa função productsControllers.js/getAll
+describe('Busca por todos produtos no banco', () => {
+  describe('Quando não existem produtos cadastrado', ()=>{
+    const res = {}
+    const req = {}
+    const nextSpy = sinon.spy() // https://imasters.com.br/front-end/testes-em-javascript-diferenca-entre-fake-spy-stub-e-mock
+    const erro = { status:404, message:'Products not found'}
+
+    beforeEach(() => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(productsServices, 'getAll').throws(erro);
+    })
+
+    afterEach(() => {
+      productsServices.getAll.restore();
+    })
+    it('é chamado o next', async()=>{
+      await productsControllers.getAll(req, res, nextSpy);
+      expect(nextSpy.calledWith(erro)).to.be.equal(true);
+    })
+  })
+  
+  describe('Quando existem produtos no banco', () => {
+    const res = {};
+    const req = {};
+
+    const mochProductsServices = [
+      {
+        "id": 1,
+        "name": "produto A",
+        "quantity": 10
+      }
+    ];
+
+    beforeEach(() => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(productsServices, 'getAll').resolves(mochProductsServices);
+    })
+
+    afterEach(() => {
+      productsServices.getAll.restore();
+    });
+
+    it(' Retorna status 200', async () => {
+      await productsControllers.getAll(req, res);
+
+      expect(res.status.calledWith(200)).to.be.equal(true);
+    });
+
+    it('Retorna um JSON com array', async () => {
+      await productsControllers.getAll(req, res);
+
+      expect(res.json.calledWith(sinon.match.array)).to.be.equal(true);
+    });
+
+  });
+});
