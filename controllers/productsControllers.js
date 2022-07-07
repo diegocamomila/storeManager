22cf232c4c485f265b846e410d011a8afb38f472
Diego Siqueira <diego.camomila@gmail.com>
2022-07-07 13:54:18 -0300

    feat: implementaçao da funçao getByID, addProduct e updateproduct em productsModels

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -9,6 +9,33 @@ const getAll = async (_req, res, next) => {
   }
 };  
   
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const product = await productsServices.getById(id);
+    return res.status(200).json(product);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const addProduct = async (req, res) => {
+  const { name } = req.body;
+  const id = await productsServices.addProduct(name); 
+  res.status(201).json({ id, name });
+};
+
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const product = { id: +id, name };
+  await productsServices.updateProduct(product);
+  res.status(200).json(product);
+};
+
 module.exports = {
   getAll,
+  getById,
+  addProduct,
+  updateProduct,
 };
