c169c964d426ebb921cf25d7d87c25c7f860a6e5
Diego Siqueira <diego.camomila@gmail.com>
2022-07-07 13:46:15 -0300

    feat: implementaçao da funçao getByID, addProduct e updateproduct em productsServices

--- services/productsServices.js
+++ services/productsServices.js
@@ -10,6 +10,34 @@ const getAll = async () => {
   return rows;
 };
 
+const getById = async (id) => {
+  const rows = await productsModels.getById(id);
+  if (!rows || null) {
+    throw treatsError(404, 'Product not found');
+  }
+  return rows;
+};
+// const products = await productsModels.getAll();
+const addProduct = async (name) => {
+  const product = await productsModels.getAll().indexOf(name) === name;
+     if (product) {
+    throw treatsError(409, 'Product already exists');
+    }
+  const createProduct = await productsModels.addProduct(name); 
+  return createProduct;
+};
+
+const updateProduct = async (name) => {
+  const product = await productsModels.getById().indexOf(name.id) === 0;
+  if (product.length === 0) {
+    throw treatsError(404, 'Product not found');
+  }
+  await productsModels.updateProduct(name);
+};
+
 module.exports = {
   getAll,
+  getById,
+  addProduct,
+  updateProduct,
 };
