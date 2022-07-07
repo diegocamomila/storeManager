ffac0545fc8a05b2ab54c0e1666051fa8fe97598
Diego Siqueira <diego.camomila@gmail.com>
2022-07-07 10:10:33 -0300

    feat: implementaçao da funçao getById em productsModels

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,6 +4,6 @@ const ProductsController = require('../controllers/productsControllers');
 const router = express.Router();
 
 router.get('/', ProductsController.getAll);
-router.get('/:id', ProductsController.getById);
+// router.get('/:id', ProductsController.getById);
 
 module.exports = router;
